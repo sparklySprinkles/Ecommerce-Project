c6462be3329303756fe37f33cc74514c1985a8a1
Isabella <izzybsparkles@gmail.com>
2024-09-25 13:48:08 -0400

    add files

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration(),
+    ProductService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
